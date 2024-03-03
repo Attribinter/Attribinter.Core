86a7efabac7c3cb15ea151caa1377a547eae7484
ErikWe <erikwestberg7@gmail.com>
2024-03-03 18:25:11 +0100

    Added generic parsers

---
+++ src/Attribinter.Core/IArgumentParser.cs
@@ -0,0 +1,14 @@
+﻿namespace Attribinter;
+
+/// <summary>Parses arguments of attributes.</summary>
+/// <typeparam name="TParameter">The type of the parameter associated with the parsed arguments.</typeparam>
+/// <typeparam name="TData">The type of the data recorded by the provided recorders.</typeparam>
+/// <typeparam name="TAttribute">The type representing the parsed attributes.</typeparam>
+public interface IArgumentParser<TParameter, TData, TAttribute>
+{
+    /// <summary>Attempts to parse some arguments of an attribute.</summary>
+    /// <param name="recorder">Records the parsed arguments.</param>
+    /// <param name="attribute">The parsed attribute.</param>
+    /// <returns>A <see cref="bool"/> indicating whether the attempt was successful.</returns>
+    public abstract bool TryParse(IArgumentRecorder<TParameter, TData> recorder, TAttribute attribute);
+}
--- src/Attribinter.Core/ITypeArgumentRecorder.cs
+++
@@ -1,5 +0,0 @@
-﻿namespace Attribinter;
-
-/// <summary>Records data about arguments of type parameters.</summary>
-/// <typeparam name="TPayload">The type of the payload passed to the recorder, describing the arguments.</typeparam>
-public interface ITypeArgumentRecorder<in TPayload> : IArgumentRecorder<ITypeParameter, TPayload> { }
