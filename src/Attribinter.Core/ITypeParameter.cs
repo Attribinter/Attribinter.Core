175b543293508e640339ca7160e44207858a6dd8
ErikWe <erikwestberg7@gmail.com>
2024-03-03 15:02:08 +0100

    Added underlying abstraction IArgumentRecorder

---
+++ src/Attribinter.Core/ITypeParameter.cs
@@ -0,0 +1,10 @@
+﻿namespace Attribinter;
+
+using Microsoft.CodeAnalysis;
+
+/// <summary>Represents a type parameter.</summary>
+public interface ITypeParameter
+{
+    /// <summary>The symbol associated with the type parameter.</summary>
+    public abstract ITypeParameterSymbol Symbol { get; }
+}
